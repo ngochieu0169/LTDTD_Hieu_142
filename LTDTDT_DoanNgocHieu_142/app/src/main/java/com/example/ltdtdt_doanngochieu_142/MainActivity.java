f79108e1ac51c1bf723f533815b0fad43ca6fc1e
ngochieu0169 <104136256+ngochieu0169@users.noreply.github.com>
2022-08-31 00:22:51 +0700

    first commit

---
+++ LTDTDT_DoanNgocHieu_142/app/src/main/java/com/example/ltdtdt_doanngochieu_142/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ltdtdt_doanngochieu_142;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
